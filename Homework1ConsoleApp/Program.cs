ea6322c0eeac7622907a0194872bd3567d5f2e64
Sreeraj Sreekumaran <SRSR@simcorp.com>
2024-10-10 09:54:39 +0800

    homework-2 committed

--- Homework1ConsoleApp/Program.cs
+++ Homework1ConsoleApp/Program.cs
@@ -12,24 +12,35 @@
         {
             var nominal = 0;
             var TradePrice = 0.0;
+            var originalPrice = 0.0;
             var currentValue = 0.0;
 
             Console.WriteLine("Input the nominal of the trade:");
-            string userInputNominal = Console.ReadLine();
+            var userInputNominal = Console.ReadLine();
             nominal = Int32.Parse(userInputNominal);
 
             Console.WriteLine("Input the Trade price of the trade:");
-            string userInputPrice = Console.ReadLine();
+            var userInputPrice = Console.ReadLine();
             TradePrice = double.Parse(userInputPrice);
 
             Console.WriteLine("Input the Trade type of the trade:");
             TransactionType trcType;
-            string userInputTransType = Console.ReadLine();
+            var userInputTransType = Console.ReadLine();
             trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInputTransType, true);
 
             currentValue = trcType == TransactionType.Buy ? (1 * nominal * TradePrice) : (-1 * nominal * TradePrice);
-
             Console.WriteLine($"Current Value for the {trcType} transaction : {currentValue}");
+
+            if (trcType == TransactionType.Sell)
+            {
+                var profitlossValue = 0.0;
+                Console.WriteLine("Input the Origial price of the trade:");
+                string userInputOrigPrice = Console.ReadLine();
+                originalPrice = double.Parse(userInputOrigPrice);
+                profitlossValue = (TradePrice - originalPrice) * nominal;
+                Console.WriteLine($"Profit Loss Value for the {trcType} transaction : {profitlossValue}");
+            }
+            
             Console.ReadLine();
         }
     }
